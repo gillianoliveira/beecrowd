8d2bced14aeacc7856125c5a8620ee2da0066222
Gillian Gomes <brgillian@gmail.com>
2024-03-07 21:28:23 -0300

    Pasta Python

---
+++ python/1000-hello-world.py
@@ -0,0 +1,12 @@
+# Plataforma: Beecrowd
+# https://judge.beecrowd.com/
+# Categoria: Iniciante
+# Problema: 1000 - Hello World!
+# https://judge.beecrowd.com/pt/problems/view/1000
+
+# Proposta
+#Você deve imprimir a mensagem "Hello World!" e em seguida o final de linha.
+
+
+# Solução
+print("Hello World!")
