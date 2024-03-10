828f1f530648f61802213fdc79959a9e4ef0d526
Gillian Gomes <brgillian@gmail.com>
2024-03-10 18:02:51 -0300

    Ass 1006 media 2 python

---
+++ python/1006-media_dois.py
@@ -0,0 +1,13 @@
+# Plataforma: Beecrowd
+# https://judge.beecrowd.com/
+# Categoria: Iniciante
+# Problema: 1006 - Média 2
+# https://judge.beecrowd.com/pt/problems/view/1006
+
+A = float(input()) * 2
+B = float(input()) * 3
+C = float(input()) * 5
+
+MEDIA = (A + B + C) / 10
+
+print("MEDIA = {:.1f}".format(MEDIA))
