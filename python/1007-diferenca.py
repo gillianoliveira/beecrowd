ab04aef5c6307acc1d555e1b05b7caedcceb094a
Gillian Gomes <brgillian@gmail.com>
2024-03-11 21:47:59 -0300

    1007 diferenca python movido para o diretorio correto

---
+++ python/1007-diferenca.py
@@ -0,0 +1,14 @@
+# Plataforma: Beecrowd
+# https://judge.beecrowd.com/
+# Categoria: Iniciante
+# Problema: 1007 - Diferença
+# https://judge.beecrowd.com/pt/problems/view/1007
+
+A = int(input())
+B = int(input())
+C = int(input())
+D = int(input())
+
+DIFERENCA = (A * B - C * D)
+
+print(f'DIFERENCA = {DIFERENCA}')
