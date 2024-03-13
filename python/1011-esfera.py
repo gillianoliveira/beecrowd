05027b58dccd95c67a680607fcedad6c65f6eb6e
Gillian Gomes <brgillian@gmail.com>
2024-03-12 22:36:22 -0300

    Esfera

---
+++ python/1011-esfera.py
@@ -0,0 +1,10 @@
+# Plataforma: Beecrowd
+# https://judge.beecrowd.com/
+# Categoria: Iniciante
+# Problema: 1011 - Esfera
+# https://judge.beecrowd.com/pt/problems/view/1011
+
+r = int(input())
+VOLUME = (4/3) * 3.14159 * (r**3)
+
+print(f'VOLUME = {VOLUME:.3f}')
