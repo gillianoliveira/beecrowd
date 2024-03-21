5538a264a714d46d269f58bb40170c69cdeffb25
Gillian Gomes <brgillian@gmail.com>
2024-03-20 21:40:34 -0300

    1016 python

---
+++ python/1016-distancia.py
@@ -0,0 +1,11 @@
+# Plataforma: Beecrowd
+# https://judge.beecrowd.com/
+# Categoria: Iniciante
+# Problema: 1016 - Distância
+# https://judge.beecrowd.com/pt/problems/view/1016
+
+
+tempo = int(input()) * 2
+
+print(f'{tempo} minutos')
+
